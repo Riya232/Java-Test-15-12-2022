52b7193c6a2f59109e58595337000261ad00798d
Riya232 <81167311+Riya232@users.noreply.github.com>
2022-12-15 19:00:34 +0530

    Update digits.java

--- digits.java
+++ digits.java
@@ -1,17 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
 
   public static void main(String[] args) {
 
     int num;
     Scanner sc=new Scanner(System.in);
+    System.out.println("enter a number: ");
     num=sc.nextInt();
     
-    int count = 0;
+    int count = 0; 
 
-    for (; num != 0; num /= 10; ++count) {
+    while (num != 0) {
+      num /= 10;
+      ++count;
     }
 
     System.out.println(count);
   }
 }
-  
